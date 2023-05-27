3859be85d79c03c34b3425fbbd83d72cdd488397
fathrahh <gazoon3@gmail.com>
2023-05-28 06:38:56 +0700

    test post for serverless

--- api/index.py
+++ api/index.py
@@ -25,26 +25,27 @@ def hello_world():
 
 
 @app.post("/api/predict")
-def predict(payload: Features) -> int:
+def predict(payload: Features):
     payload = payload.dict()
 
-    with open('model.pkl', 'rb') as f:
-        model = pickle.load(f)
+    return payload
+    # with open('model.pkl', 'rb') as f:
+    #     model = pickle.load(f)
 
-    X_dict = {
-        'gender': payload['gender'],
-        'age': payload['age'],
-        'hypertension': payload['hypertension'],
-        'heart_disease': payload['heartDisease'],
-        'ever_married': payload['everMarried'],
-        'work_type': payload['workType'],
-        'residence_type': payload['residentType'],
-        'avg_glucose_level': payload['avgGlucoseLevel'],
-        'bmi': payload['bmi'],
-        'smoking_status': payload['smokingStatus']
-    }
+    # X_dict = {
+    #     'gender': payload['gender'],
+    #     'age': payload['age'],
+    #     'hypertension': payload['hypertension'],
+    #     'heart_disease': payload['heartDisease'],
+    #     'ever_married': payload['everMarried'],
+    #     'work_type': payload['workType'],
+    #     'residence_type': payload['residentType'],
+    #     'avg_glucose_level': payload['avgGlucoseLevel'],
+    #     'bmi': payload['bmi'],
+    #     'smoking_status': payload['smokingStatus']
+    # }
 
-    X_test = np.array(list(X_dict.values()))
-    y_pred = model.predict(X_test.reshape(-1, len(X_test)))
+    # X_test = np.array(list(X_dict.values()))
+    # y_pred = model.predict(X_test.reshape(-1, len(X_test)))
 
-    return y_pred
+    # return y_pred
